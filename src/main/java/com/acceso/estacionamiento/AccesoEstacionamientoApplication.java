29707fb7b41223768c2fa831ea20d922a5a9dd3c
AlexTcw <etaverac429alumno.uaemex.mx>
2023-09-17 21:11:49 -0600

    Primer commit

---
+++ src/main/java/com/acceso/estacionamiento/AccesoEstacionamientoApplication.java
@@ -0,0 +1,13 @@
+package com.acceso.estacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoEstacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoEstacionamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acceso/estacionamiento/AccesoEstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acceso.estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoEstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
